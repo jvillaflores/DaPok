e6ab5e06bafc8cc73053fbe153af69be357027f8
jvillaflores <jvillaflores_180000001215@uic.edu.ph>
2022-04-06 15:38:52 +0800

    Fixed Image Uploading Issue

--- components/main/ChatbotAdd.js
+++ components/main/ChatbotAdd.js
@@ -10,13 +10,10 @@ import {
   ScrollView,
   Alert,
   Platform,
-  Image
-
+  Image,
 } from "react-native";
 
-import {
-  TextInput
-} from 'react-native-paper'
+import { TextInput } from "react-native-paper";
 import { connect } from "react-redux";
 import firebase from "firebase";
 require("firebase/firestore");
@@ -25,56 +22,55 @@ import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityI
 import { useValidation } from "react-native-form-validator";
 import * as FileSystem from "expo-file-system";
 import * as DocumentPicker from "expo-document-picker";
-import { Audio } from 'expo-av';
-import * as ImagePicker from 'expo-image-picker';
-
+import { Audio } from "expo-av";
+import * as ImagePicker from "expo-image-picker";
 
 function AddWord({ currentUser, route, navigation }) {
   const [loading, setLoading] = useState(false);
   const { data } = route?.params ?? {};
 
   const [bisaya, setBisaya] = useState("");
-  const [ newLanguage, setNewLanguage ] = useState("");
+  const [newLanguage, setNewLanguage] = useState("");
   const [wordID, setWordID] = useState(makeid());
   const [datalist, setDatalist] = useState("");
   const [image, setImage] = useState(null);
   const [hasGalleryPermission, setHasGalleryPermission] = useState(null);
-  
-    useEffect(() => {
-      (async () => {
-        const galleryStatus =
-          await ImagePicker.requestMediaLibraryPermissionsAsync();
-          setHasGalleryPermission(galleryStatus.status === "granted");
-      })();
-    }, []);
 
-    const pickImage = async () => {
-      let result = await ImagePicker.launchImageLibraryAsync({
-        mediaTypes: ImagePicker.MediaTypeOptions.Images,
-        allowsEditing: true,
-        aspect: [1,1],
-        quality: 1,
-      });
-      
-      if (!result.cancelled) {
-        setImage(result.uri);
-        
-      }
-    };
-    const { validate, isFieldInError, getErrorsInField, getErrorMessages } =
+  useEffect(() => {
+    (async () => {
+      const galleryStatus =
+        await ImagePicker.requestMediaLibraryPermissionsAsync();
+      setHasGalleryPermission(galleryStatus.status === "granted");
+    })();
+  }, []);
+
+  const pickImage = async () => {
+    let image = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1,
+    });
+
+    if (!image.cancelled) {
+      setImage(image.uri);
+      console.log(image.uri);
+    }
+  };
+  const { validate, isFieldInError, getErrorsInField, getErrorMessages } =
     useValidation({
       state: { newLanguage },
     });
   const onSubmit = () => {
-      validate({
-        bisaya:{required:true},
-        newLanguage:{required:true},
-      });
-      uploadImage();
-    };
+    validate({
+      bisaya: { required: true },
+      newLanguage: { required: true },
+    });
+    uploadImage();
+  };
 
   const uploadImage = async () => {
-    const uri = route.params.image;
+    const uri = image;
     const childPath = `post/${
       firebase.auth().currentUser.uid
     }/${Math.random().toString(36)}`;
@@ -105,12 +101,7 @@ function AddWord({ currentUser, route, navigation }) {
     task.on("state_changed", taskProgress, taskError, taskCompleted);
   };
 
- 
-
-//image
-
-
-
+  //image
 
   useEffect(() => {
     setDatalist(currentUser);
@@ -135,7 +126,7 @@ function AddWord({ currentUser, route, navigation }) {
     return unsubscribe;
   }, [navigation]);
 
-function makeid() {
+  function makeid() {
     var randomText = "";
     var possible =
       "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -148,119 +139,147 @@ function makeid() {
     return randomText;
   }
 
-
   const SavePostData = (downloadURL) => {
-      firebase
-        .firestore()
-        .collection("userAllChatbotAnswers")
-        .doc(firebase.auth().currentUser.uid)
-        .collection("userChatbotAnswers")
-        .doc(wordID)
-        .set({
-          wordId: wordID,
-          email: currentUser.email,
-          language:datalist.language,
-          downloadURL,
-          bisaya: data?.bisaya,
-          newLanguage,
-          status: "0",
-          upload: "1",
-          creation: firebase.firestore.FieldValue.serverTimestamp(),
-        })
-        .then(function () {
-          alert("Daghang Salamat sa imohang kontribusyon!!");
-          setLoading(null);
-          navigation.popToTop();
-        });
-        
-    };
-    const saveAllPostData = () => {
-      firebase
-        .firestore()
-        .collection("words")
-        .add({
-          uid: firebase.auth().currentUser.uid,
-          wordId: wordID,
-          email: currentUser.email,
-          username: currentUser.name,
-          language:datalist.language,
-          bisaya:data?.bisaya,
-          newLanguage,
-          audio,
-          image,
-          upload: "1",
-          creation: firebase.firestore.FieldValue.serverTimestamp(),
-        })
-        .then(function () {
-          alert("Daghang Salamat sa imohang kontribusyon!!");
-          setLoading(null);
-          navigation.popToTop();
-        });
-    };
+    firebase
+      .firestore()
+      .collection("userAllChatbotAnswers")
+      .doc(firebase.auth().currentUser.uid)
+      .collection("userChatbotAnswers")
+      .doc(wordID)
+      .set({
+        wordId: wordID,
+        email: currentUser.email,
+        language: datalist.language,
+        downloadURL: downloadURL,
+        bisaya: data?.bisaya,
+        newLanguage,
+        status: "0",
+        upload: "1",
+        creation: firebase.firestore.FieldValue.serverTimestamp(),
+      })
+      .then(function () {
+        alert("Daghang Salamat sa imohang kontribusyon!!");
+        setLoading(null);
+        navigation.popToTop();
+      });
+  };
+  const saveAllPostData = () => {
+    firebase
+      .firestore()
+      .collection("words")
+      .add({
+        uid: firebase.auth().currentUser.uid,
+        wordId: wordID,
+        email: currentUser.email,
+        username: currentUser.name,
+        language: datalist.language,
+        bisaya: data?.bisaya,
+        newLanguage,
+        audio,
+        image,
+        upload: "1",
+        creation: firebase.firestore.FieldValue.serverTimestamp(),
+      })
+      .then(function () {
+        alert("Daghang Salamat sa imohang kontribusyon!!");
+        setLoading(null);
+        navigation.popToTop();
+      });
+  };
 
-
-   {
+  {
     return (
       <ScrollView style={styles.container}>
         <View>
           <View style={styles.center}>
-            <Text style={{fontSize:18}}>Itubag kini nga panguatana sa {datalist.language}</Text>
+            <Text style={{ fontSize: 18 }}>
+              Itubag kini nga panguatana sa {datalist.language}
+            </Text>
             <Text style={styles.text}>{data?.bisaya} </Text>
           </View>
           <View style={styles.horiz}>
-              <TextInput
-                  multiline={false}
-                  activeUnderlineColor="#215A88"
-                  onChangeText={(newLanguage) => setNewLanguage(newLanguage)}/>
+            <TextInput
+              multiline={false}
+              activeUnderlineColor="#215A88"
+              onChangeText={(newLanguage) => setNewLanguage(newLanguage)}
+            />
           </View>
-          
-       
-        <View style={styles.paddingLeft}>
-          <Text style={styles.title_text}>Hulagway(Imahe)</Text>
-          <Text style={styles.guidelines}>
-            Pwede nimo butangan ug hulagway kung unsa ang iyahang nawong.
-          </Text>
-        </View>
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <TouchableOpacity
-            style={styles.imageButton}
-            onPress={pickImage}
-            title="Pick an image from camera roll">
+
+          <View style={styles.paddingLeft}>
+            <Text style={styles.title_text}>Hulagway(Imahe)</Text>
+            <Text style={styles.guidelines}>
+              Pwede nimo butangan ug hulagway kung unsa ang iyahang nawong.
+            </Text>
+          </View>
+          <View
+            style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+          >
+            <TouchableOpacity
+              style={styles.imageButton}
+              onPress={pickImage}
+              title="Pick an image from camera roll"
+            >
               <MaterialCommunityIcons
-                  style={styles.addImage}
-                  name="plus-box"
-                  color={"#707070"}
-                  size={26}
-                />
-          </TouchableOpacity>
-      {/* <Button style={styles.audioButton} title="Pick an image from camera roll" onPress={pickImage} /> */}
-      {image && <Image source={{ uri: image }} style={{ width: 200, height: 200, marginTop: 20 }} />}
-    </View>
+                style={styles.addImage}
+                name="plus-box"
+                color={"#707070"}
+                size={26}
+              />
+            </TouchableOpacity>
+            {/* <Button style={styles.audioButton} title="Pick an image from camera roll" onPress={pickImage} /> */}
+            {image && (
+              <Image
+                source={{ uri: image }}
+                style={{ width: 200, height: 200, marginTop: 20 }}
+              />
+            )}
+          </View>
           <View style={styles.horiz}>
-
-
-          
-              <TouchableOpacity onPress={()=>{onSubmit()}}
-                  style={[styles.buttonVocab,{
-                     backgroundColor: "#215A88",}]}>
-                  <Text style={{
-                     color:"#ffffff",
-                     alignSelf:'center',
-                     fontSize: 18}}>itigom</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={[styles.buttonVocab,{
-                     backgroundColor: "#e7e7e7",}]}>
-                  <Text style={{
-                     color:"#215A88",
-                     alignSelf:'center',
-                     fontWeight:'700',
-                     fontSize: 18}}>kanselahon</Text>
-              </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => {
+                onSubmit();
+              }}
+              style={[
+                styles.buttonVocab,
+                {
+                  backgroundColor: "#215A88",
+                },
+              ]}
+            >
+              <Text
+                style={{
+                  color: "#ffffff",
+                  alignSelf: "center",
+                  fontSize: 18,
+                }}
+              >
+                itigom
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={[
+                styles.buttonVocab,
+                {
+                  backgroundColor: "#e7e7e7",
+                },
+              ]}
+            >
+              <Text
+                style={{
+                  color: "#215A88",
+                  alignSelf: "center",
+                  fontWeight: "700",
+                  fontSize: 18,
+                }}
+              >
+                kanselahon
+              </Text>
+            </TouchableOpacity>
           </View>
         </View>
       </ScrollView>
     );
-  } 
+  }
 }
 
 const mapStateToProps = (store) => ({
@@ -271,32 +290,30 @@ const mapStateToProps = (store) => ({
 export default connect(mapStateToProps, null)(AddWord);
 const styles = StyleSheet.create({
   container: {
-    flex:1,
-    paddingVertical:70,
-    paddingHorizontal:10,
-
+    flex: 1,
+    paddingVertical: 70,
+    paddingHorizontal: 10,
   },
-  center:{
-    alignItems:"center",
+  center: {
+    alignItems: "center",
   },
   horiz: {
     paddingHorizontal: 40,
-    paddingVertical:20,
-    
+    paddingVertical: 20,
   },
   buttonVocab: {
     alignSelf: "center",
     borderRadius: 20,
     paddingVertical: 20,
     marginTop: 20,
-    width: "100%",  
-    paddingHorizontal:20,
+    width: "100%",
+    paddingHorizontal: 20,
   },
-  text:{
-    fontWeight:'bold',
-    fontSize:20,
-    justifyContent:'center'
-  }, 
+  text: {
+    fontWeight: "bold",
+    fontSize: 20,
+    justifyContent: "center",
+  },
   title_text: {
     //alignContent:"flex-start",
     fontWeight: "bold",
@@ -339,9 +356,9 @@ const styles = StyleSheet.create({
     fontStyle: "italic",
     color: "#707070",
   },
-  addImage:{
+  addImage: {
     flex: 1,
-    position: 'absolute',
-    marginTop: -15 
-  }
+    position: "absolute",
+    marginTop: -15,
+  },
 });
