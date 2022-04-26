6234ea23d31ede24f7c95a7c18c8dca3d0cba41f
jvillaflores <jvillaflores_180000001215@uic.edu.ph>
2022-04-26 09:51:48 +0800

    edited design

--- components/main/ChatbotAnswers.js
+++ components/main/ChatbotAnswers.js
@@ -54,19 +54,20 @@ useEffect(() => {
         onPress={() => navigation.navigate('ChatbotDone',{data:item})}>
           {/* ///////////////CDone////////////////// */}
         <View style={{ flexDirection: "row", flex: 1 }}>
-          <View style={styles.itemBody}>
-            <Text>{item?.language}</Text>
-          </View>
-          <View style={{ flexDirection: "column"}}>
-            <View style={styles.itemBody}>
+          
+          <View style={{ flex:1, flexDirection: "column"}}>
+            
+            <View style={[styles.itemBody,]}>
               <Text style={styles.itemsName}>{item?.bisaya}</Text>
             </View>
-            <View style={styles.itemBody}>
+            <View style={[styles.itemBody,]}>
               <Text>{item?.newLanguage}</Text>
             </View> 
-
           </View>
           
+          <View style={[styles.itemBody]}>
+            <Text>{item?.language}</Text>
+          </View>
         </View>
           </TouchableOpacity>
     );
@@ -157,14 +158,17 @@ const styles = StyleSheet.create({
     },
   
     itemBody: {
-      flex: 1,
-      paddingHorizontal: 5,
+     
+      
+      paddingHorizontal: 3,
       justifyContent: "center",
+      
     },
   
     itemsName: {
       fontWeight:'bold',
       fontSize: 16,
+      textAlign:'left'
     },
     itemStatus: {
       backgroundColor: "#69C080",
